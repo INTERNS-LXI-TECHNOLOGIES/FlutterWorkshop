4215e8a157567ac95656aedc321c844137ebdb27
ajilsajeev <ajil.sajeev.c@lxisoft.com>
2022-06-15 00:10:29 +0530

    modified

--- pet_store/lib/screens/pets_screen.dart
+++ pet_store/lib/screens/pets_screen.dart
@@ -1,10 +1,6 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pet_store/screens/pet_details_screen.dart';
 
-import '../bloc/pet_list/pets_bloc.dart';
-import '../widgets/image_decode_widget.dart';
+import '../widgets/pet_list_status.dart';
 
 class PetsScreen extends StatefulWidget {
   const PetsScreen({Key? key}) : super(key: key);
@@ -51,59 +47,15 @@ class _PetsScreenState extends State<PetsScreen>
       //DefaultTabController.of(context)!.index
       body: TabBarView(controller: controller, children: [
         Container(
-          child: body(context, controller.index),
+          child: petListByStatus(context, controller.index),
         ),
         Container(
-          child: body(context, controller.index),
+          child: petListByStatus(context, controller.index),
         ),
         Container(
-          child: body(context, controller.index),
+          child: petListByStatus(context, controller.index),
         ),
       ]),
     );
-  }
-
-  body(context, int index) {
-    debugPrint('tab controller index => $index');
-    List<String> statusList = ['available', 'pending', 'sold'];
-    BlocProvider.of<PetsBloc>(context).add(LoadPetsByStatus(statusList[index]));
-    return BlocBuilder<PetsBloc, PetsState>(
-      builder: (context, state) {
-        // if (state is PetsLoading) {
-        //   return const Center(child: CircularProgressIndicator());
-        // }
-        if (state is PetsLoaded && state.petList.isNotEmpty) {
-          return ListView.builder(
-            shrinkWrap: true,
-            itemCount: state.petList.length,
-            itemBuilder: (context, i) => ListTile(
-              title: state.petList[i].name!.isNotEmpty
-                  ? Text(state.petList[i].name!)
-                  : const Text('no name'),
-              leading: CircleAvatar(
-                child:
-                    getImagenBase64(petPhotos(state.petList[i].photoUrls)[0]),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PetDetails(pet: state.petList[i]),
-                  ),
-                );
-              },
-            ),
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    );
-  }
-
-  List<String> petPhotos(BuiltList<String> photos) {
-    List<String> photosUrl = [];
-    photosUrl.addAll(photos);
-    return photosUrl;
   }
 }
---
+++ pet_store/lib/widgets/pet_list_status.dart
@@ -0,0 +1,50 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/pet_list/pets_bloc.dart';
+import '../screens/pet_details_screen.dart';
+import 'image_decode_widget.dart';
+
+petListByStatus(context, int index) {
+  debugPrint('tab controller index => $index');
+  List<String> statusList = ['available', 'pending', 'sold'];
+  BlocProvider.of<PetsBloc>(context).add(LoadPetsByStatus(statusList[index]));
+  return BlocBuilder<PetsBloc, PetsState>(
+    builder: (context, state) {
+      // if (state is PetsLoading) {
+      //   return const Center(child: CircularProgressIndicator());
+      // }
+      if (state is PetsLoaded && state.petList.isNotEmpty) {
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: state.petList.length,
+          itemBuilder: (context, i) => ListTile(
+            title: state.petList[i].name!.isNotEmpty
+                ? Text(state.petList[i].name!)
+                : const Text('no name'),
+            leading: CircleAvatar(
+              child: getImagenBase64(petPhotos(state.petList[i].photoUrls)[0]),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PetDetails(pet: state.petList[i]),
+                ),
+              );
+            },
+          ),
+        );
+      } else {
+        return const Center(child: CircularProgressIndicator());
+      }
+    },
+  );
+}
+
+List<String> petPhotos(BuiltList<String> photos) {
+  List<String> photosUrl = [];
+  photosUrl.addAll(photos);
+  return photosUrl;
+}
